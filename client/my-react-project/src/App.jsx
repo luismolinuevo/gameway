63225a0c5886fa1f34a517dc6143d3e709c6b9fe
evnul000 <81979106+evnul000@users.noreply.github.com>
2023-03-30 03:53:16 -0400

    login page register pagestill under devel

--- client/my-react-project/src/App.jsx
+++ client/my-react-project/src/App.jsx
@@ -1,4 +1,5 @@
-
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-     
+     <Register/>
     </div>
   )
 }
