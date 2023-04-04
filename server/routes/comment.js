cee0c1e3ed203eb78201c223861529694eaca91d
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-04-04 16:01:26 -0400

    get comment for post, and create comment under post routes created

---
+++ server/routes/comment.js
@@ -0,0 +1,41 @@
+import prisma from "../db/index.js";
+import express from "express";
+
+export default function createAuthRouter(passport) {
+    const router = express.Router();
+
+    router.get("/:postId", async (req, res) => {
+        const postId = req.params.postId;
+
+        const commentForPost = await prisma.comment.findMany({
+            where: {
+                postId: Number(postId)
+            }
+        });
+
+        res.status(200).json({
+            success: true,
+            commentForPost
+        })
+
+    });
+
+    router.post("/:postId", async (req, res) => {
+        const postId = req.params.postId;
+
+        const createPost = await prisma.comment.create({
+            data: {
+                postId: Number(postId),
+                text: req.body.text
+            }
+        });
+
+        res.status(201).json({
+            success: true,
+        })
+    })
+
+
+
+    return router;
+}
