a6764a1cb89c8d6f7f51110c4c2a3822eadc27c6
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-05-02 17:21:03 -0400

    created get following route and tested

--- server/routes/follower.js
+++ server/routes/follower.js
@@ -4,8 +4,8 @@ import prisma from "../db/index.js";
 export default function createFollowerRouter(passport) {
   const router = express.Router();
 
+  //get all people following the user
   router.get("/followingme", async (req, res) => {
-    console.log("ing")
     const getFollowers = await prisma.follower.findMany({
         where: {
             followingId: req.user.id
@@ -15,8 +15,23 @@ export default function createFollowerRouter(passport) {
     res.status(200).json({
         success: true,
         getFollowers
+    });
+  });
+
+  router.get("/following", async (req, res) => {
+    const getFollowing = await prisma.follower.findMany({
+      where: {
+        userId: req.user.id
+      }
+    });
+
+    res.status(200).json({
+      success: true,
+      getFollowing
     })
-  })
+  });
+
+
 
   return router;
 }
