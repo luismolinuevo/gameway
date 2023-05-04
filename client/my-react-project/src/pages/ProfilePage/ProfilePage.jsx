b733ab17ccf2a2f8c2a418971fce2bb4f55bde93
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-05-04 16:20:08 -0400

    profile page router created

--- client/my-react-project/src/pages/ProfilePage/ProfilePage.jsx
+++ client/my-react-project/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar.jsx";
+import { FaUserAstronaut } from "react-icons/fa";
 
 export default function ProfilePage() {
   return (
-    <div>ProfilePage</div>
-  )
+    <div>
+      <div className="account">
+        <Navbar />
+        <div className="account-info-container">
+          <div className="account-info-left">
+            <div className="user-icon">
+              <div className="user-container">
+                <FaUserAstronaut style={{ color: "yellow" }} size={180} />
+              </div>
+            </div>
+            <p className="user-name">Tom</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
