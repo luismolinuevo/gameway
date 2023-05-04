77802764bb03e23f99d7424e2ad93b9ebdbd536a
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-05-04 16:46:57 -0400

    top part of page created

--- client/my-react-project/src/App.jsx
+++ client/my-react-project/src/App.jsx
@@ -1,35 +1,29 @@
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-import Homepage from './pages/Homepage/Homepage'
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Homepage from "./pages/Homepage/Homepage";
 
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import UserHome from './pages/UserHome/UserHome'
-import Account from './pages/AccountPage/AccountPage'
-import Games from './pages/Games/games'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserHome from "./pages/UserHome/UserHome";
+import Account from "./pages/AccountPage/AccountPage";
+import Games from "./pages/Games/games";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 function App() {
-
-
   return (
     <Router>
-
       <div className="App">
-       
         <Routes>
-        <Route exact path="/" element={<Homepage/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/userHome" element={<UserHome/>}/>
-        <Route path="/account" element={<Account/>}/>
-        <Route path="/games" element={<Games />} />
-        <Route path="/profile" element={<ProfilePage/>}/>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/userHome" element={<UserHome />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/profile" element={<ProfilePage />} /> {/*TODO need to make/add params to this */}
         </Routes>
-      
       </div>
     </Router>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- client/my-react-project/src/pages/ProfilePage/ProfilePage.jsx
+++ client/my-react-project/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import { FaUserAstronaut } from "react-icons/fa";
+import "./ProfilePage.scss";
 
 export default function ProfilePage() {
   return (
     <div>
-      <div className="account">
+      <div className="profile">
         <Navbar />
-        <div className="account-info-container">
-          <div className="account-info-left">
-            <div className="user-icon">
-              <div className="user-container">
+        <div className="profile-info-container">
+          <div className="profile-info-left">
+            <div className="profile-user-icon">
+              <div className="profile-user-container">
                 <FaUserAstronaut style={{ color: "yellow" }} size={180} />
               </div>
             </div>
-            <p className="user-name">Tom</p>
+            <div className="profile-actions">
+              <p className="profile-user-name">Tom</p>
+              <div>
+                <button className="profile-buttons">
+                    Follow
+                </button>
+                <button className="profile-buttons">
+                    Message
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
