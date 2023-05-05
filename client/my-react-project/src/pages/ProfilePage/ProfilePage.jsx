434772d59631ddb7dc5fcf629ab826f4d60cb4cd
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-05-05 17:25:46 -0400

    basic layout created

--- client/my-react-project/src/pages/ProfilePage/ProfilePage.jsx
+++ client/my-react-project/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,54 +1,59 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import { FaUserAstronaut } from "react-icons/fa";
 import "./ProfilePage.scss";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { BsFillPeopleFill } from "react-icons/bs";
+
 
 export default function ProfilePage() {
-    const params = useParams();
-    const [userInfo, setUserInfo] = useState([]);
-    const [userPost, setUserPost] = useState([]);
+  const params = useParams();
+  const [userInfo, setUserInfo] = useState([]);
+  const [userPost, setUserPost] = useState([]);
 
-    //TODO make params work , fetch all post, maybe see followers, and following
-    //TODO show all post with button
-    //TODO make buttons work
+  //TODO make params work , fetch all post, maybe see followers, and following
+  //TODO show all post with button
+  //TODO make buttons work
 
+  useEffect(() => {
+    const findUser = async () => {
+      try {
+        const user = await axios.get(`http://localhost:8080/auth/${2}`);
 
-    useEffect(() => {
-
-        const findUser =  async () => {
-            try {
-                const user = await axios.get(`http://localhost:8080/auth/${2}`);
-
-                if(user.status == 200) {
-                    setUserInfo(user.data);
-                } else {
-                    console.log("Error fetching data")
-                }
-            } catch(error) {
-                console.log(error)
-            }
+        if (user.status == 200) {
+          setUserInfo(user.data);
+        } else {
+          console.log("Error fetching data");
         }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        const userPost = async () => {
-          try {
-            const userPost = await axios.get(`http://localhost:8080/post/userspost/${2}`);
-            if(userPost.status == 200) {
-              setUserPost(userPost.data);
-              console.log(userPost.data)
-            }
-          } catch(error) {
-            console.log("Error fetching user post")
-          }
+    const userPost = async () => {
+      try {
+        const userPost = await axios.get(
+          `http://localhost:8080/post/userspost/${2}`
+        );
+        if (userPost.status == 200) {
+          setUserPost(userPost.data);
+          console.log(userPost.data);
         }
+      } catch (error) {
+        console.log("Error fetching user post");
+      }
+    };
 
-        findUser();
-        userPost();
+    findUser();
+    userPost();
 
-        return () => {
-        };
-      }, []);
+    return () => {};
+  }, []);
+
+  const handleFollow = () => {
+    
+  }
 
   return (
     <div>
@@ -62,16 +67,27 @@ export default function ProfilePage() {
               </div>
             </div>
             <div className="profile-actions">
-              <p className="profile-user-name">{userInfo.username}</p>
               <div>
-                <button className="profile-buttons">
-                    Follow
-                </button>
-                <button className="profile-buttons">
-                    Message
-                </button>
+                <p className="profile-user-name">{userInfo.username}</p>
+                <div>
+                  <button className="profile-buttons">Follow</button>
+                  <button className="profile-buttons">Message</button>
+                </div>
+              </div>
+              <div className="following">
+                <div className="follower-actions">
+                  <p>Following</p>
+                  <Link to=""><BsFillPeopleFill className="follower-icons" /></Link>
+                </div>
+                <div className="follower-actions">
+                  <p>Followers</p>
+                  <Link to=""><BsFillPeopleFill className="follower-icons" /></Link>
+                </div>
               </div>
             </div>
+          </div>
+          <div className="see-post-container ">
+            <button className="see-post">See Post</button>
           </div>
         </div>
       </div>
