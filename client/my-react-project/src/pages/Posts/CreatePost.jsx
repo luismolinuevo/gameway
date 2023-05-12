2a559bbaa143c203e7203211afc23949907d6c17
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-05-12 02:29:43 -0400

    Finished with the create post page. If you want you can make some changes on the design.

--- client/my-react-project/src/pages/Posts/CreatePost.jsx
+++ client/my-react-project/src/pages/Posts/CreatePost.jsx
@@ -3,18 +3,26 @@ import {useState} from "react";
 import axios from "axios";
 import "./createpost.scss";
 import { useLocation } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+
+
 
 export default function CreatePost(){
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const { search } = useLocation();
   const params = new URLSearchParams(search);
-  const gameName = params.get("name");
-
+  let gameName = params.get("name");
+  console.log(gameName);
+  if(gameName == "" || gameName == null || gameName == "null"){
+    gameName = "Music"
+  }
+  console.log(gameName);
   async function handleSubmit(){
+    event.preventDefault()
     try{
-      const token = localeStorage.getItem(jwt);
-      const response = axios.post('https://localhost:8080/post', {
+      const token = localStorage.getItem("token");
+      const response = axios.post('http://localhost:8080/post', {
         'title': title, 
         'body': body, 
         'game': gameName,
@@ -33,12 +41,27 @@ export default function CreatePost(){
   }
   
   return(
-    <form >
-      <label for="title" className="title">Post Title</label>
-      <input type="text" className="title--input" onChange={e => setTitle(e.target.value)}></input>
-      <label for="title" className="title">Post Body</label>
-      <input type="text" className="title--body" onChange={e => setBody(e.target.value)}></input>
-      <button className="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <div className="div createPost">
+        <Navbar/>
+        <form >
+            
+            <div className="form--title">
+                <label htmlFor="title" className="title">Post Title</label>
+                <input type="text" className="title--input" onChange={e => setTitle(e.target.value)}></input>
+            </div>
+            
+            <div className="form--body">
+                <label htmlFor="title" className="body--title">Post Body</label>
+                <br/>
+                <textarea className="body--form" onChange={e => setBody(e.target.value)}></textarea>
+            </div>
+            
+            <div className="submit">
+                <button className="submit--button"onClick={handleSubmit}>Submit</button>
+            </div>
+            
+        </form>
+    </div>
+    
   );
 }
