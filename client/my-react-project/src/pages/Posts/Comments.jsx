e3fa78107f015a834bc322770e961c088efa8e13
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2023-05-30 15:13:31 -0400

    Refixed some of the things I deleted on git

---
+++ client/my-react-project/src/pages/Posts/Comments.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './comments.scss';
+import Navbar from '../../components/Navbar/Navbar'
+
+/*
+    The layout of this page would be similar to that of a blog post
+    The page is going to start out with the post contents
+    then we are going to have the section where they are going to create the post
+    then we are going to show all of the posts below
+*/
+
+/*
+This is going to be to create the Post Card for the comments page
+It is basically going to be the same as the PostCard function minus the 
+*/
+
+export default function Comments(){
+
+
+    return (
+        <div className="comments--main">
+            <Navbar />
+            <p>Hello World</p>
+        </div>
+        
+    );
+}
--- client/my-react-project/src/pages/Posts/PostCard.jsx
+++ client/my-react-project/src/pages/Posts/PostCard.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import './postcard.scss'
-import { Link } from "react-router-dom";
 
 export default function PostCard(props){
     const [userName, setUserName] = useState("");
 
     useEffect(() =>{
         async function getUsername(){
-            const response = await axios.get('http://localhost:8080/auth/user/' + props.userId);
-            const user = response.data.username;
-            console.log(user);
+            const token = localStorage.getItem("token");
+            const response = await axios.get('http://localhost:8080/auth/login',{
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            const user = response.data.data.username;
             setUserName(user);
         }
         getUsername();
@@ -21,29 +24,23 @@ export default function PostCard(props){
 
     return(
         <div className="div postCard">
-            {/* Replace the link located here with the comment page of the post in question */}
-            <Link className="sidebar--link" to="/games">
-                <ul className="card--list">
-                    <div className="card--title">
-                        <li>
-                            {userName}
-                        </li>
-                        <li>
-                            {props.game}
-                        </li>
-                    </div>
-                    <div className="card--body">
-                        <li>
-                            Title: {props.title}
-                        </li>
-                        <li>
-                            {props.body}
-                        </li>
-                    </div>
-                    
-                    
-                </ul>
-            </Link>
+            <ul className="card--list">
+                <div className="card--title">
+                    <li>
+                        {userName}
+                    </li>
+                    <li>
+                        {props.title}
+                    </li>
+                </div>
+                <li>
+                    {props.game}
+                </li>
+                <li>
+                    {props.body}
+                </li>
+                
+            </ul>
         </div>
     )
 }
